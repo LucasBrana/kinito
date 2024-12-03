753f6bea7a05a16dd4735bdf826044857971c076
Lucas Brana <l.brana@net.estia.fr>
2024-12-03 11:00:45 +0000

    feat: implement thread-safe ping-pong functionality with mutex synchronization

--- main.cpp
+++ main.cpp
@@ -3,19 +3,38 @@
 // LED pour la boucle principale
 DigitalOut led(LED1);
 
-// Fonction pour afficher "Ping" 100 fois
+// Mutex pour synchroniser les threads
+Mutex mutex_ping_pong;
+
+// Fonction pour afficher "Ping"
 void ping() {
-    for (int i = 0; i < 100; i++) {
+    /*for (int i = 0; i < 100; i++) {
+        mutex_ping_pong.lock(); // Verrouiller avant l'accès partagé
         printf("Ping\n");
-        ThisThread::sleep_for(10ms);
+        mutex_ping_pong.unlock(); // Déverrouiller pour permettre à "Pong" de s'exécuter
+        ThisThread::sleep_for(10ms); // Petite pause pour simuler un délai
+    }*/
+    while (true) {
+        mutex_ping_pong.lock(); // Verrouiller avant l'accès partagé
+        printf("Ping\n");
+        mutex_ping_pong.unlock(); // Déverrouiller pour permettre à "Pong" de s'exécuter
+        ThisThread::sleep_for(250ms); // Petite pause pour simuler un délai
     }
 }
 
-// Fonction pour afficher "Pong" 100 fois
+// Fonction pour afficher "Pong"
 void pong() {
-    for (int i = 0; i < 100; i++) {
+    /*for (int i = 0; i < 100; i++) {
+        mutex_ping_pong.lock(); // Verrouiller avant l'accès partagé
         printf("Pong\n");
-        ThisThread::sleep_for(10ms);
+        mutex_ping_pong.unlock(); // Déverrouiller pour permettre à "Ping" de s'exécuter
+        ThisThread::sleep_for(10ms); // Petite pause pour simuler un délai
+    }*/
+    while (true) {
+        mutex_ping_pong.lock(); // Verrouiller avant l'accès partagé
+        printf("Pong\n");
+        mutex_ping_pong.unlock(); // Déverrouiller pour permettre à "Ping" de s'exécuter
+        ThisThread::sleep_for(250ms); // Petite pause pour simuler un délai
     }
 }
 
@@ -31,7 +50,9 @@ int main() {
     // Boucle principale
     while (true) {
         led = !led; // Inverser l'état de la LED
+        mutex_ping_pong.lock(); // Verrouiller avant l'accès partagé
         printf("Alive!\n");
+        mutex_ping_pong.unlock(); // Déverrouiller pour permettre à "Ping" de s'exécuter
         ThisThread::sleep_for(500ms); // Attendre 500 ms avant le prochain clignotement
     }
 }
